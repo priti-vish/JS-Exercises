29e73b8a86e3b2e5854acc8910d941b949acf8e7
priti-vish <priti.vish1221@gmail.com>
2026-04-12 14:45:18 +0530

    Remove the unncessary files from main

--- continuous-hello.js
+++
@@ -1,8 +0,0 @@
-const prompt = require("prompt-sync")();
-let number=1
-
-
-do{
-    number = Number(prompt("Enter a number to print or  to stop enter 0-zero: "))
-    console.log("hello")
-}while(number!==0)
